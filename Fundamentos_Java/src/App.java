91f1b282584f83b217e95004181a4d937b97e68e
CaioMTRodrigues <caiom.rodrigues@hotmail.com>
2024-09-21 16:56:22 -0300

    Soma a+b

---
+++ Fundamentos_Java/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+         //println -> para pular linha
+        int a,b;
+        a = 10;
+        b = 20;
+        int c = a +b;
+        System.out.println("Valor de C é: "+ c);
+    }
+}
